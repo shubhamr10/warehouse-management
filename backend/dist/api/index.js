88750ff8481756e92216818e238b6038536193b4
shubhamr10 <shubhamrauniyar10@gmail.com>
2024-08-11 10:07:01 +0530

    Remove dist from the git

--- backend/dist/api/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-exports.default = () => {
-    const app = (0, express_1.Router)();
-    // all routers here
-    return app;
-};
